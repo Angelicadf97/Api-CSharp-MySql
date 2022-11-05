17b51720b3133095582714fd1bac447e0e9d76b0
Angélica Flores <99211551+Angelicadf97@users.noreply.github.com>
2022-11-05 15:40:49 -0300

    Adicionar arquivos de projeto.

---
+++ Api-agencia/Controllers/ClienteController.cs
@@ -0,0 +1,80 @@
+﻿using Api_agencia.Data;
+using Api_agencia.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+
+namespace Api_agencia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+        public ClienteController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cursos - LISTA TODOS OS CURSOS
+        [HttpGet]
+        public IEnumerable<Cliente> GetClientes()
+        {
+            return _context.Clientes;
+        }
+
+        // GET: api/Cursos/id - LISTA CURSO POR ID
+        [HttpGet("{id}")]
+        public IActionResult GetClientePorId(long id)
+        {
+            Cliente cliente = _context.Clientes.SingleOrDefault(modelo => modelo.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(cliente);
+        }
+
+        // CRIA UM NOVO CURSO
+        [HttpPost]
+        public IActionResult CriarCliente(Cliente item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            _context.Clientes.Add(item);
+            _context.SaveChanges();
+            return new ObjectResult(item);
+        }
+
+        // ATUALIZA UM CURSO EXISTENTE
+        [HttpPut("{id}")]
+        public IActionResult AtualizaCliente(long id, Cliente item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        // APAGA UM CURSO POR ID
+        [HttpDelete("{id}")]
+        public IActionResult DeletaCliente(long id)
+        {
+            var cliente = _context.Clientes.SingleOrDefault(m => m.Id == id);
+            if (cliente == null)
+            {
+                return BadRequest();
+            }
+            _context.Clientes.Remove(cliente);
+            _context.SaveChanges();
+            return Ok(cliente);
+        }
+    }
+}
---
+++ Api-agencia/Models/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_agencia.Models
+{
+    [Table("cliente")]
+    public class Cliente
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o cpf do cliente")]
+        [StringLength(14)]
+        public String Cpf { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do cliente")]
+        [StringLength(100)]
+        public String Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o telefone do cliente")]
+        [StringLength(15)]
+        public String Tel { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha do cliente")]
+        [StringLength(20)]
+        public String Senha { get; set; }
+
+        [Required(ErrorMessage = "Informe o email do cliente")]
+        [StringLength(100)]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "Informe o logradouro do cliente")]
+        [StringLength(100)]
+        public String Logradouro { get; set; }
+
+        [Required(ErrorMessage = "Informe a cidade do cliente")]
+        [StringLength(100)]
+        public String Cidade { get; set; }
+    }
+}
